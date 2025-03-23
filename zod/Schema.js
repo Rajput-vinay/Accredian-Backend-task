f771540e711596c907ff3f828daadfe7d6a46fab
Vinay Rajput <105452265+Rajput-vinay@users.noreply.github.com>
2025-03-23 22:32:13 +0530

    Update Schema.js

--- zod/Schema.js
+++ zod/Schema.js
@@ -5,7 +5,7 @@ const {z} = require('zod')
  const SubmitReferralSchema = z.object({
 referrer_name: z.string().min(3, "Require minimum length must be 3"), 
 referrer_email: z.string().email("Invalid email format"),
-referred_name: z.string().min(3, "Require minimum length must be 3"), 
+referred_name: z.string().min(4, "Require minimum length must be 4"), 
 referred_email: z.string().email("Invalid email format"),
   message: z.string().optional(),
   
